294fa1325d64ca74fb71877c0cda16e5e3bd33c1
kenshinku <nattawat57353.soo@gmail.com>
2017-03-23 01:34:03 +0700

    a

--- app.js
+++ app.js
@@ -7,24 +7,28 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-// parse application/x-www-form-urlencoded
+// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
-// parse application/json
+// Process application/json
 app.use(bodyParser.json())
 
-// index
+// Index route
 app.get('/', function (req, res) {
-  res.send('hello world i am a secret bot')
+  res.send('Hello world, I am a chat bot')
 })
 
-// for facebook verification
+// for Facebook verification
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === "this_is_my_token") {
+  if (req.query['hub.verify_token'] === 'this_is_my_token') {
     res.send(req.query['hub.challenge'])
-  } else {
-    res.send('Error, wrong token')
   }
+  res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'))
 })
 
 
