04b54328c479576446285e04c1b2554b0c5f575d
kenshinku <nattawat57353.soo@gmail.com>
2017-03-23 00:54:47 +0700

    ''

--- app.js
+++ app.js
@@ -28,14 +28,13 @@ app.get("/webhook", function (req, res) {
 // All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function (req, res) {
   // Make sure this is a page subscription
-  console.error(req.body);
   if (req.body.object == "page") {
     // Iterate over each entry
     // There may be multiple entries if batched
     req.body.entry.forEach(function(entry) {
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-        if (event.message) {
+        if (event.postback) {
           processPostback(event);
         }
       });
@@ -47,8 +46,31 @@ app.post("/webhook", function (req, res) {
 
 function processPostback(event) {
   var senderId = event.sender.id;
-  var text = event.message.text;
-  sendMessage(senderId, {text});
+  var payload = event.postback.payload;
+
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
 }
 
 // sends message to user
