40a9573e59135be67e837e3c67e5883f4954e9cb
kenshinku <nattawat57353.soo@gmail.com>
2017-03-25 22:09:30 +0700

    fix text

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get('/webhooks', function (req, res) {
 app.post('/webhooks', function (req, res) {
   var entry = FB.getMessageEntry(req.body)
   // IS THE ENTRY A VALID MESSAGE?
+  console.log(entry)
   if (entry && entry.message) {
     if (entry.message.attachments) {
       // NOT SMART ENOUGH FOR ATTACHMENTS YET
